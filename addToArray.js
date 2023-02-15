1c81e490399b99e5d12a2a570752b73a3103f131
Hsouley <57496297+Hsouley@users.noreply.github.com>
2023-02-15 08:46:14 +0100

    Add files via upload

---
+++ addToArray.js
@@ -0,0 +1,3 @@
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["C1",1]);
+console.log(myArray);
---
+++ myStr.js
@@ -0,0 +1,3 @@
+var begin ="Ceci est le debut";
+var end ="Ceci est la fin";
+console.log(begin+' '+end);
